386203f9d1563674eb545b02d992362dd125723b
jelenam86 <jelena.mat86@gmail.com>
2019-06-14 22:23:28 +0200

    Added comparator and me tot he main method

--- src/exercises/JobCandidate.java
+++ src/exercises/JobCandidate.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 import java.util.List;
 
-public class JobCandidate {
+public class JobCandidate implements Comparable<JobCandidate> {
 
 	private List<String> personalityTraits;
 	private int hoursSpentCoding;
@@ -24,9 +24,52 @@ public class JobCandidate {
 		this.teacherName = teacherName;
 		this.salaryRequired = salaryRequired;
 	}
-	
+
 	public static void main(String[] args) {
-		JobCandidate june = new JobCandidate(Arrays.asList(new String[] {"adaptable", "scatty", "curious"}), 650, "Vinny Wade", 1200000); 
+		JobCandidate june = new JobCandidate(Arrays.asList(new String[] { "adaptable", "scatty", "curious" }), 650,
+				"Vinny Wade", 1200000);
+		JobCandidate me = new JobCandidate(
+				Arrays.asList(new String[] { "fast-learner", "goal-oriented", "persistant" }), 60, "June Clark",
+				100000);
+
+		if (me.compareTo(june) >= 0)
+			System.out.println("Awesome! Maybe you can get a job somewhere...");
+		else
+			System.out.println("Sorry. Maybe next time.");
+	}
+
+	@Override
+	public int compareTo(JobCandidate o) {
+		int hours = this.hoursSpentCoding - o.hoursSpentCoding;
+		int salary = this.salaryRequired - o.salaryRequired;
+		int bonus = bonus(this) - bonus(o);
+		if (hours == 0 && salary == 0 && bonus == 0)
+			return 0;
+		else if (hours > 0 && bonus > 0)
+			return 1;
+		else
+			return -1;
+	}
+
+	private static int bonus(JobCandidate jc) {
+		int bonus = 0;
+		if (jc.personalityTraits.size() > 5)
+			bonus++;
+		if (jc.teacherName.equals("June Clark"))
+			bonus += 5;
+		if (jc.hoursSpentCoding > 600)
+			bonus++;
+		if (jc.salaryRequired < 150000)
+			bonus++;
+		if (jc.personalityTraits.contains("persistant"))
+			bonus++;
+		if (jc.personalityTraits.contains("adaptable"))
+			bonus++;
+		if (jc.personalityTraits.contains("confident"))
+			bonus++;
+		if (jc.personalityTraits.contains("responsible"))
+			bonus++;
+		return bonus;
 	}
 
 }
