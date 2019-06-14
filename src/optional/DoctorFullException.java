09b5f0cb2508bde792fa5766a5a5a249a0111576
jelenam86 <jelena.mat86@gmail.com>
2019-06-14 22:36:31 +0200

    Added customized exception for the Doctor class

---
+++ src/optional/DoctorFullException.java
@@ -0,0 +1,13 @@
+package optional;
+
+public class DoctorFullException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DoctorFullException() {
+		super("Doctor is full!");
+	}
+}
